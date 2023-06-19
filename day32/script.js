e411e38d9e6b618c9cee7433f6e66350e7b10201
mockingbitch <jarvis.phongtran@gmail.com>
2023-06-19 21:18:56 +0700

    day32

---
+++ day32/script.js
@@ -0,0 +1,33 @@
+// Object Json
+var Duck = {
+    "fullName": "Hoang Minh Duck",
+    "age": "18",
+    "course": "K12-FullstackOverflow"
+}
+
+// Array thông thường
+var Duck = [
+    "Hoang Minh Duck", "18", "K12-FullstackOverflow"
+]
+
+//Object json in Array
+var students = [
+    {
+      "Duck": {
+        "fullName": "Hoang Minh Duck",
+        "age": "18",
+        "course": "K12-FullstackOverflow"
+      }
+    },
+    {
+      "Cuong": {
+        "fullName": "Mạnh Cường Ngủyên",
+        "age": "18",
+        "course": "K13-Fullstack"
+      }
+    }
+  ]
+
+// Json bao gồm các cặp "key": "value" được đóng gói bởi cặp ngoặc {} và ngăn cách bởi dấu ,
+
+
